aa8af94c1f72e7304ee1f7afbed5cd5ae4d9f34c
nishacodes <nishabatra27@gmail.com>
2013-12-10 18:10:44 -0500

    move parser to its own file

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ Bundler.require
 require './lib/artist.rb'
 require './lib/genre.rb'
 require './lib/song.rb'
+require './lib/parser.rb'
 
 
 module Playlister
@@ -27,45 +28,12 @@ module Playlister
       end
     end
 
-    get 'genre/:name' do
-      @name = params[:name]
+    get '/genre/:name' do
+      @genre_name = params[:name]
+      genre = Genre.detect(@genre_name)
       erb :genre_detail
     end
 
 
-  end
-
-  class Parser
-    attr_accessor :song_list, :song_list_delimit
-
-    def initialize
-      @song_list = Dir.entries("./public/data").select {|f| !File.directory? f}
-      @song_list_delimit
-      delimit
-      create_objects
-    end
-
-    def delimit
-      @song_list_delimit = @song_list.map do |song|
-        song.split(/\s-\s|\s\[|\].mp3/)
-      end 
-    end
-
-    def create_objects
-      # songarray looks like this [artist, song, genre]
-      @song_list_delimit.each do |songarray|
-        Artist.find_or_create(songarray[0]).tap do |a|
-          song = Song.new.tap do |s|
-            s.name = songarray[1]
-            genre = Genre.find_or_create(songarray[2]).tap do |g|
-              g.artists
-            end
-            s.genre = genre
-        end
-          a.add_song(song)
-          a.genres
-        end
-      end
-    end
   end
 end
